b92d7f2780f735d3e59430717834af6a3ad95921
Lukasz <szati95@wp.pl>
2023-04-03 23:48:30 +0200

    posptroccesing + filtering directories

--- Application/Postprocessing2.py
+++ Application/Postprocessing2.py
@@ -10,17 +10,23 @@ sim_number = 97
 result = []
 
 
-def sorting_key(arg):
-    z = re.split('sim|_', arg[0:5])
-    print(z)
+# def sorting_key(arg):
+#     z = re.split('sim|_', arg[0:5])
+#     print(z)
 
+def sorting_key(arg):
+    return arg[0:5]
+
+i = 0
 for j in range(1, simulation_batches_number+1):
     batch_folder_name = f'{ROOT_DIR}\simulations_{j}'
     path = os.listdir(batch_folder_name)
-    # for sim in path:
-    #     # f = os.path.join(batch_folder_name, sim)
-    # z = path.sort(key=sorting_key)
-    # for x in path:
-    #     print(re.split('sim|_', x))
-    # z = path.sort(key=lambda x: int(re.split('sim|_', x)[1]))
-    print(sorted(path, key=sorting_key))
+    for filename in sorted(map(lambda x: re.split('sim|_', x)[1], path), key=int):
+        with open(f'{ROOT_DIR}\simulations_{j}\sim{filename}_2_summary', 'r') as f:
+            head = next(f.read() for _ in range(9))
+            head_splitted = re.split('\n', head)
+            print(head_splitted)
+            i+=1
+
+print(i)
+
