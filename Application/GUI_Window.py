7b01724d493fcf02419ca3a5d00034bcc39d9155
Lukasz <lukasz.szatan.95@gmail.com>
2023-05-16 00:14:06 +0200

    GUI added + added plot and legend

--- Application/GUI_Window.py
+++ Application/GUI_Window.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+from matplotlib import patches
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 import tkinter as tk
@@ -24,13 +26,45 @@ class Window(object):
     def column_graph(self):
         self.fig = Figure(figsize=(12, 7.6), dpi=100)
         self.ax = self.fig.add_subplot(111)
-        # df = {'x_pos': [1, 1], 'y_pos': [2, 3], 'color': 'red'}
-        # self.ax.plot(smaller_range_limits_enu)
-        # self.ax.scatter('x_pos', 'y_pos', data=df1, c='color')
-        self.add_plot_to_column_graph(smaller_range_limits_enu, 'red')
-        self.add_plot_to_column_graph(range_limits_enu, 'blue')
-        self.add_plot_to_column_graph(range_sea_limits_enu, 'green')
-        self.add_plot_to_column_graph(range_shore_enu, 'brown')
+        self.ax.set_aspect('equal', adjustable='box')
+
+
+
+
+        self.add_plot_to_column_graph(smaller_range_limits_enu, 'red', 'EPD53 A')
+        self.add_plot_to_column_graph(range_limits_enu, 'blue', 'EPD53 B')
+        self.add_plot_to_column_graph(range_sea_limits_enu, 'green', 'P-24 i P-20')
+        self.add_plot_to_column_graph(range_shore_enu, 'brown', 'Brzeg')
+        self.ax.plot((main.launchpad_launch_vector[0], main.launchpad_launch_vector[2]),
+                     (main.launchpad_launch_vector[1], main.launchpad_launch_vector[3]), color='grey', linewidth=0.5)
+
+
+
+        self.add_scatter_to_column_graph(postprocessing.create_impact_points()[0], 'red', 'Rakieta')
+        self.add_scatter_to_column_graph(postprocessing.create_impact_points()[1], 'green', 'Payload')
+        self.add_scatter_to_column_graph(postprocessing.create_impact_points()[2], 'blue', 'Booster')
+
+        circle1 = plt.Circle((0, 0), 1.0, color='black', fill=False, label='Miejsce Startu')
+        self.ax.add_patch(circle1)
+
+        # for i in range(len(postprocessing.sim_calculations())):
+        #     self.ax.add_patch(patches.Ellipse(
+        #         (postprocessing.mean()[i][0]/1e3, postprocessing.mean()[i][1]/1e3),
+        #         postprocessing.sim_calculations()[i]['sim_crossrange_stdev']/1e3,
+        #         postprocessing.sim_calculations()[i]['sim_downrange_stdev']/1e3,
+        #         360-postprocessing.downgrade_line_theta()[i],
+        #         alpha=0.3))
+        # self.ax.add_patch(patches.Ellipse(
+        #     (postprocessing.mean()[1][0]/1e3, postprocessing.mean()[0][1]/1e3),
+        #     postprocessing.sim_calculations()[1]['sim_crossrange_stdev']/1e3,
+        #     postprocessing.sim_calculations()[1]['sim_downrange_stdev']/1e3,
+        #     360-postprocessing.downgrade_line_theta()[1],
+        #     alpha=1))
+        #
+
+
+
+        self.ax.legend()
         self.chart = FigureCanvasTkAgg(self.fig, master=self.root)
         self.chart.draw()
         self.chart.get_tk_widget().pack(side=tk.TOP, expand=1)
@@ -39,17 +73,21 @@ class Window(object):
         toolbar = NavigationToolbar2Tk(self.chart, self.root)
         toolbar.update()
 
-    def add_scatter_to_column_graph(self, data):
-        df1 = {'x_pos': [5, 5], 'y_pos': [7, data], 'color': 'green'}
-        self.ax.scatter('x_pos', 'y_pos', data=df1, c='color')
+    def add_scatter_to_column_graph(self, data, color_kind, label):
+        x = []
+        y = []
+        for k in data:
+            x.append(k[0] / 1e3)
+            y.append(k[1] / 1e3)
+        self.ax.scatter(x, y, label=label, facecolors='none', edgecolors=color_kind)
 
-    def add_plot_to_column_graph(self, data, color_kind):
+    def add_plot_to_column_graph(self, data, color_kind, label_kind):
         x = []
         y = []
         for k in data:
             x.append(k[0]/1e3)
             y.append(k[1]/1e3)
-        self.ax.plot(x, y, color=color_kind, linewidth=0.5)
+        self.ax.plot(x, y, color=color_kind, linewidth=0.5, label=label_kind)
 
 
 zap1 = Window()
